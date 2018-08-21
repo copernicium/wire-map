a5d7e989b1cf1d87ce1d64f3a69acc00861fc2d9
copernicium <logan.traffas@gmail.com>
2018-08-21 14:48:54 -0700

    Add sample code snippet

--- parser/include/parser.hpp
+++ parser/include/parser.hpp
@@ -1,5 +1,12 @@
 #pragma once
 
+#include <optional>
+#include <pair>
+#include <string>
+
 namespace wiremap::parser{
-    
+    using ParseInput = std::string;
+
+    template<typename R>
+    using ParseResult = std::optional<std::pair<R,std::string>>;
 }
