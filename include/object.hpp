87959d67c147f1678ebbd66850a6b77ce42ef551
copernicium <logan.traffas@gmail.com>
2018-08-22 21:14:31 -0700

    Add default value to Object and replace enable_if with static_assert

--- include/object.hpp
+++ include/object.hpp
@@ -3,20 +3,66 @@
 #include <array>
 
 namespace wiremap{
-    template<typename T>
-    struct Object{
-        T value;
-        T default_value; //TODO
-        bool valid;
+    namespace detail{
+        struct ObjectBase{};
 
-        Object(T v,T default_v):value(v),default_value(default_v),valid(true){}
-        Object(T v)noexcept:value(v), valid(true){}
-        Object()noexcept:valid(false){}
+        template<typename T>
+        struct MaybeObject: public ObjectBase{
+            using value_type = T;
+
+            T value;
+            bool valid;
+
+            MaybeObject(T v)noexcept: value(v), valid(true){}
+            MaybeObject()noexcept: valid(false){}
+        };
+
+        template<typename T, std::size_t Size, bool ValueTypeTest>
+        struct Container: public detail::MaybeObject<T>{
+            std::size_t size;
+
+            Container(T v)noexcept: detail::MaybeObject<T>(v){
+                static_assert(ValueTypeTest, "Container value type not derived from detail::ObjectBase");
+                size = Size;
+            }
+
+            Container()noexcept: detail::MaybeObject<T>(){
+                static_assert(ValueTypeTest, "Container value type not derived from detail::ObjectBase");
+                size = Size;
+            }
+        };
+    }
+
+    template<typename T, typename S, S DefaultValue>
+    struct Object: public detail::MaybeObject<T>{
+        T default_value;
+
+        Object(T v, T default_v)noexcept: detail::MaybeObject<T>(v){
+            static_assert(std::is_convertible_v<T,S>, "Default value different than value type");
+            default_value = default_v;
+        }
+        Object(T v)noexcept: detail::MaybeObject<T>(v){
+            static_assert(std::is_convertible_v<T,S>, "Default value different than value type");
+            default_value = DefaultValue;
+        }
+        Object()noexcept: detail::MaybeObject<T>(){
+            static_assert(std::is_convertible_v<T,S>, "Default value different than value type");
+            default_value = DefaultValue;
+        }
     };
 
-    template<typename T,
-             typename = std::enable_if<std::is_integral_v<T>>>
-    struct Integral : Object<T> {};
+    template<typename T>
+    struct Integral: public Object<T, int, 0> {
+        Integral(T v, T default_v)noexcept: Object<T,int,0>(v, default_v){
+            static_assert(std::is_integral_v<T>, "Integral defined with non-integral type");
+        }
+        Integral(T v)noexcept: Object<T,int,0>(v){
+            static_assert(std::is_integral_v<T>, "Integral defined with non-integral type");
+        }
+        Integral()noexcept: Object<T,int,0>(){
+            static_assert(std::is_integral_v<T>, "Integral defined with non-integral type");
+        }
+    };
 
     using Bit = Integral<bool>;
     using Char = Integral<char>;
@@ -26,11 +72,12 @@ namespace wiremap{
     using QWord = Integral<uint64_t>;
     using Integer = Integral<long>;
 
-    using Bool = Object<bool>;
-    using Real = Object<double>;
+    using Bool = Object<bool, bool, false>;
+    using Real = Object<double, int, 0>;
 
     template<typename T, std::size_t Size>
-    using List = Object<std::array<Object<T>, Size>>;
-    template<typename ... T>
-    using Collection = Object<std::tuple<Object<T>...>>;
+    using List = detail::Container<std::array<T, Size>, Size, std::is_base_of_v<detail::ObjectBase, T>>;
+
+    template<typename ...T>
+    using Collection = detail::Container<std::tuple<T...>, sizeof...(T), std::conjunction_v<std::is_base_of<detail::ObjectBase, T>...>>;
 }
---
+++ tests/object_test.cpp
@@ -0,0 +1,21 @@
+#include "gtest/gtest.h"
+#include "object.hpp"
+
+using namespace wiremap;
+
+TEST(ObjectTest, Constructor){
+    Object<int,int,0> a;
+    EXPECT_EQ(a.default_value, 0);
+    EXPECT_EQ(a.valid, false);
+
+    Object<double,int,0> b = 6;
+    EXPECT_EQ(b.default_value, 0);
+    EXPECT_EQ(b.value, 6);
+    EXPECT_EQ(b.valid, true);
+}
+
+TEST(ContainerTest, Constructor){
+    List<Integer, 4> ints;
+    Collection<Integer, Real> t;
+    EXPECT_EQ(0,0); //as long as this compiles it passes
+}
