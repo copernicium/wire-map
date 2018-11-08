1f675920a31a7c692706bc1d6cf6d7c07f42309a
copernicium <logan.traffas@gmail.com>
2018-11-08 13:18:24 -0800

    Better separate Device set-up benchmarks

--- benchmarks/device_benchmark.cpp
+++ benchmarks/device_benchmark.cpp
@@ -8,77 +8,88 @@
 
 using namespace wiremap;
 
+const auto ROBORIO_HASH = hashstr("roborio");
+const auto PWM1_HASH = hashstr("pulse_width_1");
+const auto SPARK_HASH = hashstr("spark1");
+const auto CURRENT_HASH = hashstr("current");
+
 void setup_sample_wiremap(){
 	WireMap::reset();
     Result r = std::function<Object(void)>([]{ return Object::primitive((Integer)5); });
     WireMap::add(
-        "roborio",
-        std::make_pair("pulse_width_1",r)
+        ROBORIO_HASH,
+        std::make_pair(PWM1_HASH,r)
 	);
 }
 
-static void BM_ParameterConstructor(benchmark::State& state) {
-	setup_sample_wiremap();
+static void BM_HashStr(benchmark::State& state){
 	for(auto _ : state){
-        Parameter p = {"roborio", "pulse_width_1"};
+		hashstr("roborio");
     }
 }
 
-static void BM_ConstantConstructor(benchmark::State& state) {
+static void BM_ParameterConstructor(benchmark::State& state){
+	setup_sample_wiremap();
+	for(auto _ : state){
+        Parameter p ={ROBORIO_HASH, PWM1_HASH};
+    }
+}
+
+static void BM_ConstantConstructor(benchmark::State& state){
     for(auto _ : state){
         Constant<Integer> c = 8;
     }
 }
 
-static void BM_ResultConstructor(benchmark::State& state) {
+static void BM_ResultConstructor(benchmark::State& state){
     for(auto _ : state){
 		Result r = std::function<Object(void)>([]{ return Object::primitive((Integer)5); });
     }
 }
 
-static void BM_DeviceConstructor0(benchmark::State& state) {
+static void BM_DeviceConstructor0(benchmark::State& state){
     for(auto _ : state){
-        Device spark1 = {};
+        Device spark1 ={};
     }
 }
 
-static void BM_DeviceConstructor1(benchmark::State& state) {
-    Parameter p = {"roborio", "pulse_width_1"};
-	auto member = std::make_pair("pulse_width_1",p);
+static void BM_DeviceConstructor1(benchmark::State& state){
+    Parameter p = {ROBORIO_HASH, PWM1_HASH};
+	auto member = std::make_pair(PWM1_HASH,p);
 
     for(auto _ : state){
-        Device spark1 = {
-			"spark1",
+        Device spark1 ={
+			SPARK_HASH,
 			member
         };
     }
 }
 
-static void BM_DeviceConstructor2(benchmark::State& state) {
+static void BM_DeviceConstructor2(benchmark::State& state){
 	setup_sample_wiremap();
-    Parameter p = {"roborio", "pulse_width_1"};
-	auto member1 = std::make_pair("pulse_width_1",p);
+    Parameter p ={ROBORIO_HASH, PWM1_HASH};
+	auto member1 = std::make_pair(PWM1_HASH,p);
 
 	Result r = std::function<Object(void)>([]{ return Object::primitive((Integer)5); });
-	auto member2 = std::make_pair("current",r);
+	auto member2 = std::make_pair(CURRENT_HASH,r);
 
     for(auto _ : state){
-        Device spark1 = {
-			"spark1",
+        Device spark1 ={
+			SPARK_HASH,
 			member1,
 			member2
         };
     }
 }
 
-static void BM_DeviceSetup(benchmark::State& state) {
+static void BM_DeviceSetup(benchmark::State& state){
     unsigned i = 0;
     for(auto _ : state){
 		Result r = std::function<Object(void)>([]{ return Object::primitive((Integer)5); });
 
         WireMap::add(
-            "spark" + std::to_string(i),
-            std::make_pair("current",r)
+            SPARK_HASH + i,
+            std::make_pair(CURRENT_HASH,r)
         );
 
         i++;
@@ -86,16 +97,18 @@ static void BM_DeviceSetup(benchmark::State& state) {
     WireMap::reset();
 }
 
-static void BM_ParameterAccess(benchmark::State& state) {
+static void BM_ParameterAccess(benchmark::State& state){
 	setup_sample_wiremap();
 
     for(auto _ : state){
-        Parameter p = {"roborio", "pulse_width_1"};
+        Parameter p ={ROBORIO_HASH, PWM1_HASH};
         Object r2 = p.get();
 
 		// std::cout << "value: "<<Object::visit(to_string, r2) << "\n";
     }
 }
+
+BENCHMARK(BM_HashStr);
 
 BENCHMARK(BM_ParameterConstructor);
 BENCHMARK(BM_ConstantConstructor);
